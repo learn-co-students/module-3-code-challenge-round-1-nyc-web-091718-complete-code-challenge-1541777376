b736f4d5d703e34b5ee96dcafe3b92c4042c8201
TurtleFeeder <t.choong@yahoo.com>
2018-11-09 11:32:39 -0500

    main features completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,80 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 1411 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+  let theImg;
+  const imgDiv = document.getElementById('image_card')
+  const imgTag = document.getElementById('image')
+  const imgName = document.getElementById('name')
+  const imgLikesSpan = document.getElementById('likes')
+  const commentForm = document.getElementById('comment_form')
+  let imgCommentsUl = document.getElementById('comments')
+// on load fetch
+  fetch(imageURL)
+    .then(resp=> resp.json())
+    .then(imgJSON => {
+      theImg = imgJSON;
+      imgTag.src = theImg.url;
+      imgName.innerText = theImg.name;
+      imgLikesSpan.innerText = theImg.like_count
+      imgCommentsUl.innerHTML = renderAllComments(theImg.comments)
+    })
+
+// event listener
+
+  imgDiv.addEventListener('click', imgDivEventHandler)
+  commentForm.addEventListener('submit', formEventHandler)
+
+// helper function
+
+  function renderAllComments(array) {
+    return array.map(comment => renderSingleCommentLi(comment)).join('')
+  }
+
+  function renderSingleCommentLi(comment) {
+    return `<li>${comment.content}</li>`;
+  }
+
+  function imgDivEventHandler(event) {
+    if (event.target.id === "like_button") {
+      let likeNum = parseInt(imgLikesSpan.innerText);
+      likeNum++
+      imgLikesSpan.innerText = likeNum;
+
+      fetch(likeURL, {
+        method: 'POST',
+        headers: {'Accept': 'application/json',
+  'Content-Type': 'application/json'},
+        body: JSON.stringify({image_id: imageId})
+      })
+    } // end ck for like btn if stmt
+  } // end imgDivEventHandler fn
+
+  function formEventHandler(event) {
+    event.preventDefault();
+    const inputComment = document.getElementById('comment_input')
+    let comment = {image_id: imageId, content: inputComment.value}
+    console.log(theImg);
+    console.log(event.target);
+    console.log(inputComment);
+    console.log(imgCommentsUl);
+    console.log(comment);
+    imgCommentsUl.innerHTML += renderSingleCommentLi(comment);
+    commentForm.reset();
+
+    fetch(commentsURL, {
+      method: 'POST',
+      headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+      body: JSON.stringify(comment)
+    })
+
+  } // end formEventHandler fn
 
 })

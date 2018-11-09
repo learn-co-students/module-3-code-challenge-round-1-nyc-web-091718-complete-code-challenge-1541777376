f2f8e5fd1f8dd0d0d19b8f16e9828ef1dadfe16c
AsafDavidov <adavidov@conncoll.edu>
2018-11-09 11:08:12 -0500

    Complete step 5

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const likesTag = document.getElementById('likes')
   const ulComments = document.getElementById('comments')
   const likeButton = document.getElementById('like_button')
+  const commentForm = document.getElementById('comment_form')
   //Step 1
   fetch(imageURL)
   .then(response=>response.json())
@@ -27,12 +28,28 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(likeURL,{
       method: "POST",
       headers: {
-        "Content-Type": "application/json; charset=utf-8"
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({image_id:1407})
     })
   })
 
+  //Step 3
+  commentForm.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let commentInput = document.getElementById('comment_input').value
+    ulComments.innerHTML += `<li>${commentInput}</li>`
+    commentForm.reset()
+    fetch(commentsURL,{
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({image_id:1407,content:commentInput})
+    })
+  })
   //Helper Methods
   function listComments(imgObj){
     return imgObj.comments.map((comment)=>listComment(comment)).join('')
